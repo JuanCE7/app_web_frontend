67f41e98d74ac6b75c7ede547bce71b4915f8d9c
JuanCE7 <juancastiestrella@gmail.com>
2024-09-16 23:01:44 -0500

    feat: configuraciones para next-auth

---
+++ src/components/ButtonAuth.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function ButtonAuth() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user?.email} <br />
+        <button
+          onClick={() => signOut()}
+        >
+          Sign out
+        </button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button
+        onClick={() => signIn()}
+      >
+        Sign in
+      </button>
+    </>
+  );
+}
